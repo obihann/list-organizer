117f118ed1716d2280a6804cb1f5f747cd650c53
Jeff Hann <jeffhann@gmail.com>
2014-06-21 18:38:27 -0300

    integrating twitter api

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var twitterAPI = require('node-twitter-api');
 var twitter = new twitterAPI({
     consumerKey: 'zbrlCUa6FLzVujrmwYIpGYtFA',
     consumerSecret: 'iRbfp6Q00hDv2jvRtWzzyAq5XklpaVfvEVJ9kcgr4Ip0OrU03m',
-    callback: 'http://yoururl.tld/something'
 });
 
 var express = require('express');
@@ -18,7 +17,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 app.get('/', function(req, res) {
-    res.render('index', { title: 'Hello, World!' })
+    res.render('index', { title: 'Hello, World!', message: "Welcome"})
+});
+
+app.get('/signin', function(req, res) {
+    twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+        if (error) {
+            res.render('index', { title: 'Get Request Token', message: JSON.stringify(error)})
+        } else {
+            res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+requestToken);
+        }
+    });
+});
+
+app.get('/callback', function(req, res) {
+    res.render('index', { title: 'Callback', message: 'Your being called back'})
+    twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+        if (error) {
+            res.render('index', { title: 'Get Request Token', message: JSON.stringify(error)})
+        } else {
+            res.render('index', { title: 'Done', message: 'Look what we did'})
+        }
+    });
 });
 
 var port = process.env.PORT || 3000;
