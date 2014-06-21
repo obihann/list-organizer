0f3181392fae32b046bcfb95b4cd133b06a1aa10
Jeff Hann <jeffhann@gmail.com>
2014-06-21 17:39:15 -0300

    starting express

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ var twitter = new twitterAPI({
     consumerSecret: 'iRbfp6Q00hDv2jvRtWzzyAq5XklpaVfvEVJ9kcgr4Ip0OrU03m',
     callback: 'http://yoururl.tld/something'
 });
+
+var express = require('express');
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+router.get('/', function(req, res) {
+        res.render('index', { title: 'Hello, World!' })
+});
