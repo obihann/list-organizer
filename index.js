083b7710528528b1827ca4723c9e35b43fd0dc52
Jeff Hann <jeffhann@gmail.com>
2014-06-21 17:32:13 -0300

    setting up project

---
+++ index.js
@@ -0,0 +1,6 @@
+var twitterAPI = require('node-twitter-api');
+var twitter = new twitterAPI({
+    consumerKey: 'zbrlCUa6FLzVujrmwYIpGYtFA',
+    consumerSecret: 'iRbfp6Q00hDv2jvRtWzzyAq5XklpaVfvEVJ9kcgr4Ip0OrU03m',
+    callback: 'http://yoururl.tld/something'
+});
